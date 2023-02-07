55477e74a50aa2c84ba1b6923b716e6b09bde9ef
ChanceMaker <cedrickbaker7@gmail.com>
2023-02-06 23:33:25 -0600

    Init

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -15,7 +15,7 @@ class QuotesSpider(scrapy.Spider):
         
     def parse(self,response):
         page = response.url.split("/")[-2]
-        filename - f'quotes-{page}.html'
+        filename = f'quotes-{page}.html'
         with open(filename,'wb') as f:
             f.write(response.body)
         self.log(f'Saved File {filename}')
